340af2042c6a4e9c7aaf18ba8e5123f5cffdeff0
kifuan <kifuan@foxmail.com>
2022-08-16 23:06:06 +0800

    feat: runtime environment

---
+++ helang/env.py
@@ -0,0 +1,35 @@
+from typing import *
+from helang.tokens import TYPES
+
+
+class Value:
+    def __init__(self, value_type: str, value: Optional[List[int]] = None):
+        if value_type not in TYPES:
+            raise TypeError('YOU CANNOT FIND ANOTHER TYPE.')
+        self.value = value
+
+
+class Env:
+    def __init__(self, base: Optional['Env'] = None):
+        self._base = base
+        self._data = dict()
+
+    def define(self, key: str, val: Value):
+        self._data[key] = val
+
+    def set(self, key: str, val: Value):
+        # Find in bases
+        cur = self
+        while cur:
+            if key in cur._data.keys():
+                cur._data[key] = val
+                break
+            cur = cur._base
+        else:
+            self._data[key] = val
+
+    def get(self, key: str) -> Value:
+        val = self._data.get(key)
+        if val is None and self._base is not None:
+            return self._base.get(key)
+        return val
